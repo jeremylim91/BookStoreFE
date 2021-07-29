8f923dbac2c8ac6aa707300268e7fe53d5fd5584
Jeremy <jeremylim_91@live.com>
2021-07-29 18:22:21 +0800

    created folder to hold all HOCs

---
+++ src/components/HOCs/PageHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="page-header">
+      <div class="container">
+      <h1>
+          <slot :class="headerContent" :name="headerContent" > This is the default header</slot>
+      </h1>
+        <v-subheader>
+            <slot :class="subheader" :name="subheader" >This is the default subheader</slot>
+        </v-subheader>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "PageHeader",
+    data(){
+        return{
+            headerContent:"header-content",
+            subheader:"subheader"
+        }
+    }
+
+}
+</script>
+
+<style>
+
+.page-header{
+background-color: #808ea3;
+color: whitesmoke;
+font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+
+.container{
+    max-width: 500px;
+    margin-left: 30px;
+}
+</style>
