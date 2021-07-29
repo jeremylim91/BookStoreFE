185835cd72fa753d1af4866d6a108f15563b915d
Jeremy <jeremylim_91@live.com>
2021-07-29 18:21:56 +0800

    added folder to contain conpoment files' :

---
+++ src/components/AllBooks/AllBooks-index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      <PageHeader>
+          <template v-slot:header-content>
+              All Books
+          </template>
+          <template v-slot:subheader>
+              View and manage all books in your bookstore
+          </template>
+      </PageHeader>
+      <AllBooksTable/>
+  </div>
+</template>
+
+<script>
+import PageHeader from '../HOCs/PageHeader.vue'
+import AllBooksTable from './AllBooks-table.vue'
+export default {
+    components:{
+        PageHeader,
+        AllBooksTable
+    },
+    data(){
+        return {
+
+        }
+    }, 
+
+}
+</script>
+
+<style>
+
+</style>
