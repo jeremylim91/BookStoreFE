72cf0f8db7e51bf756b90be2944b5d97bffc0824
Jeremy <jeremylim_91@live.com>
2021-07-29 18:22:44 +0800

    added routes to allBooks and search

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import allBooksIndex from '../components/AllBooks/AllBooks-index'
 
 Vue.use(VueRouter)
 
@@ -17,6 +18,15 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/allBooks',
+    name: 'All Books',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // component: () => import('../components/AllBooks/allBooks-index.vue')
+    component: allBooksIndex
   }
 ]
 
