a660c30c48f073477dec7e055878a5496a106a78
Jeremy <jeremylim_91@live.com>
2021-07-30 18:35:31 +0800

    added search component

---
+++ src/components/Search/SearchBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <v-container>
+      <v-form v-model="valid" class="form" @submit.prevent="getSearchResults">
+        <v-row>
+          <v-col cols="8" offset="1">
+            <v-text-field
+              v-model="searchString"
+              :rules="nameRules"
+              label="Search for a book"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="2">
+            <v-btn type="submit">
+              Search!
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-form>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+import { GET_SEARCH_RESULTS } from "../../store/moduleSearch";
+export default {
+  data: () => ({
+    valid: false,
+    searchString: "",
+    nameRules: [(v) => !!v || "Try entering some text to search for books"],
+  }),
+  methods: {
+    ...mapActions("moduleSearch", [GET_SEARCH_RESULTS]),
+  },
+  computed: {
+    ...mapGetters("moduleSearch", { searchResults: GET_SEARCH_RESULTS }),
+  },
+};
+</script>
+
+<style scoped>
+.form {
+  width: 100%;
+}
+</style>
---
+++ src/components/Search/SearchIndex.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+       <PageHeader class="test-header">
+          <template v-slot:header-content>
+              Search
+          </template>
+          <template v-slot:subheader>
+              Find books by title or content
+          </template>
+      </PageHeader>
+    <SearchBar/>
+      
+  </div>
+</template>
+
+<script>
+import PageHeader from '../HOCs/PageHeader.vue'
+import SearchBar from './SearchBar.vue'
+export default {
+    components:{
+        PageHeader,
+        SearchBar,
+    },
+    data(){
+        return {
+
+        }
+    }, 
+
+}
+</script>
+
+<style>
+
+</style>
