8a848a3c5671f462ce67ebcfd3be8e4377922098
Jeremy <jeremylim_91@live.com>
2021-08-02 18:43:14 +0800

    changed the slot names to fit recommended style i vue doucmentation

--- src/components/HOCs/PageHeader.vue
+++ src/components/HOCs/PageHeader.vue
@@ -1,39 +1,41 @@
 <template>
   <div class="page-header">
-      <div class="container">
+    <div class="container">
       <h1>
-          <slot :class="headerContent" :name="headerContent" > This is the default header</slot>
+        <slot :class="headerContent" name="headerContent">
+          This is the default header</slot
+        >
       </h1>
-        <v-subheader>
-            <slot :class="subheader" :name="subheader" >This is the default subheader</slot>
-        </v-subheader>
+      <v-subheader>
+        <slot :class="subheader" name="subheader"
+          >This is the default subheader</slot
+        >
+      </v-subheader>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: "PageHeader",
-    data(){
-        return{
-            headerContent:"header-content",
-            subheader:"subheader"
-        }
-    }
-
-}
+  name: "PageHeader",
+  data() {
+    return {
+      headerContent: "header-content",
+      subheader: "subheader",
+    };
+  },
+};
 </script>
 
 <style scoped>
-
-.page-header{
-background-color: #808ea3;
-color: whitesmoke;
-font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+.page-header {
+  background-color: #808ea3;
+  color: whitesmoke;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
 }
 
-.container{
-    max-width: 500px;
-    margin-left: 30px;
+.container {
+  max-width: 500px;
+  margin-left: 30px;
 }
 </style>
