a6ff8d965083dc71d821057b8c33777f7dbf2454
Jeremy <jeremylim_91@live.com>
2021-08-02 18:44:28 +0800

    added media query to make page responsive. Also changed reference to slot names in v-slot, to align with best practies in vue docs

--- src/components/AllBooks/AllBooksIndex.vue
+++ src/components/AllBooks/AllBooksIndex.vue
@@ -1,45 +1,42 @@
 <template>
   <div>
-      <PageHeader class="test-header">
-          <template v-slot:header-content>
-              All Books
-          </template>
-          <template v-slot:subheader>
-              View and manage all books in your store
-          </template>
-      </PageHeader>
-      <AllBooksTable/>
+    <PageHeader class="test-header">
+      <template v-slot:headerContent>
+        All Books
+      </template>
+      <template v-slot:subheader>
+        View and manage all books in your store
+      </template>
+    </PageHeader>
+    <AllBooksTable />
   </div>
 </template>
 
 <script>
-import PageHeader from '../HOCs/PageHeader.vue'
-import AllBooksTable from './AllBooksTable.vue'
+import PageHeader from "../HOCs/PageHeader.vue";
+import AllBooksTable from "./AllBooksTable.vue";
 export default {
-    components:{
-        PageHeader,
-        AllBooksTable
-    },
-    data(){
-        return {
-
-        }
-    }, 
-
-}
+  components: {
+    PageHeader,
+    AllBooksTable,
+  },
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style>
-.test-header{
-    /* position: absolute; */
-    position: sticky;
-    top:0;
-    z-index: 1;
-    width: 100%;
+.test-header {
+  /* position: absolute; */
+  position: sticky;
+  top: 0;
+  z-index: 1;
+  width: 100%;
 }
 @media only screen and (max-width: 960px) {
-    .test-header{
-        display: none;
-    }
+  .test-header {
+    display: none;
+  }
 }
 </style>
