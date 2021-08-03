acb229b4106eab6d59d535f2d6a006c541bbc2d5
Jeremy <jeremylim_91@live.com>
2021-08-03 18:20:06 +0800

    refactored code

---
+++ src/components/HOCs/EditBtn.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-btn
+    class="mx-auto my-3"
+    block
+    color="primary"
+    elevation="3"
+    dark
+    @click="openDialog"
+  >
+    <v-icon left>
+      mdi-pencil
+    </v-icon>
+    Edit
+  </v-btn>
+</template>
+
+<script>
+export default {
+  props: ["openDialog"],
+};
+</script>
+
+<style></style>
