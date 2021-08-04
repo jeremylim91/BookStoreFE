8a2ecb317a4a5bb1332ea47b84c67af7644d09b5
jeremylim91 <jeremylim_91@live.com>
2021-08-04 23:38:11 +0800

    created store module to handle book update

---
+++ src/store/moduleUpdateBook.mjs
@@ -0,0 +1,33 @@
+import axios from './axiosConfigs.js';
+
+export default {
+  namespaced: true,
+  state: () => ({
+    newBook: {},
+  }),
+  mutations: {
+    // updateBook(state, data) {
+    //   console.log(`data in mutations after updating book:`);
+    //   console.log(data);
+    //   // update the new book state
+    //   state.newBook = data;
+    // },
+  },
+  actions: {
+    updateBook({ state, commit, rootState }, payload) {
+      axios
+        .put('/books/updateBook', payload)
+        .then(({ data }) => {
+          // commit to getAllBooks data so that the data on the page is refreshed
+          commit('moduleAllBooks/getAllBooksData', data, { root: true });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    toggleIsBookAdded({ state, commit, rootState }) {
+      commit('toggleIsBookAdded');
+    },
+  },
+  getters: {},
+};
