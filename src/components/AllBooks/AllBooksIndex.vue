c2152b240f91a763c7e8f6f33fff57c0e01289b7
jeremylim91 <jeremylim_91@live.com>
2021-08-04 23:40:37 +0800

    added code to use the multistep form to edit book details

--- src/components/AllBooks/AllBooksIndex.vue
+++ src/components/AllBooks/AllBooksIndex.vue
@@ -8,158 +8,142 @@
         View and manage all books in your store
       </template>
     </PageHeader>
-    <AllBooksTable :openDialog="openDialog" />
+    <AllBooksTable
+      :open-dialog="openDialog"
+      :update-selected-book="updateSelectedBook"
+    />
     <!-- <v-layout row justify-center> -->
     <v-dialog v-model="showDialog" persistent max-width="70%">
       <template v-slot:activator="{ on }">
         <!-- <v-btn color="primary" dark v-on="on">Open Dialog</v-btn> -->
       </template>
-      <v-card>
-        <!-- <v-card-title class="headline"
-          >Use Google's location service?</v-card-title
-        >
-        <v-card-text
-          >Let Google help apps determine location. This means sending anonymous
-          location data to Google, even when no apps are running.</v-card-text
-        > -->
-        <template v-if="showForm">
-          <v-card-title>
-            Create a new book entry
-          </v-card-title>
-
-          <v-form ref="form" v-model="valid" class="form" lazy-validation>
-            <v-card-text>
-              <v-text-field
-                v-model="title"
-                :rules="rules"
-                counter="25"
-                hint="This field uses counter prop"
-                label="Book title"
-              ></v-text-field>
-              <v-text-field
-                v-model="thumbnailUrl"
-                hint="Please enter a valid URL"
-                label="URL to book image"
-              ></v-text-field>
-
-              <v-text-field
-                v-model="authors"
-                :rules="rules"
-                counter="25"
-                hint="This field uses counter prop"
-                label="Authors"
-              ></v-text-field>
-              <v-text-field
-                v-model="price"
-                :rules="numRules"
-                counter="25"
-                hint="Please enter a valid integer"
-                label="Price (SGD)"
-              ></v-text-field>
-              <v-text-field
-                v-model="shortDescription"
-                :rules="rules"
-                counter="25"
-                hint="This field uses counter prop"
-                label="Description"
-              ></v-text-field>
-            </v-card-text>
-          </v-form>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <!-- Handle cancellation -->
-            <v-btn color="green darken-1" plain @click="showDialog = false"
-              >Cancel</v-btn
-            >
-            <!-- Trigger the preview to display -->
-            <v-btn
-              type="submit"
-              color="green darken-1"
-              :disabled="!valid"
-              @click="validate"
-              plain
-              >Preview</v-btn
-            >
-          </v-card-actions>
+      <MultiStepForm
+        ref="multiStepForm"
+        :is-form-submitted="isFormSubmitted"
+        :prep-data-and-submit="prepDataAndSubmit"
+        :title="title"
+        :authors="authors"
+        :price="price"
+        :short-description="shortDescription"
+        :thumbnail-url="thumbnailUrl"
+        :reset-form-fields="resetFormFields"
+      >
+        <template v-slot:title>
+          <v-text-field
+            v-model.trim="title"
+            :rules="textRules"
+            counter="50"
+            label="Book title"
+          />
         </template>
-        <!-- Handle the preview -->
-        <template v-else>
-          <Card>
-            <template v-slot:image>
-              <v-img
-                :aspect-ratio="16 / 9"
-                contain
-                :src="thumbnailUrl"
-                class="imageClass"
-              />
-            </template>
-            <template v-slot:title>
-              {{ title }}
-            </template>
-            <template v-slot:subheader>
-              {{ authors }}
-            </template>
-            <template v-slot:price>
-              {{ price }}
-            </template>
-            <template v-slot:description>
-              {{ shortDescription }}
-            </template>
-          </Card>
-
-          <v-card-title> Preview </v-card-title>
-
-          <v-card-text>
-            blah blah
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <!-- Handle back-->
-            <v-btn color="green darken-1" plain @click="showForm = true"
-              >Back/Edit</v-btn
-            >
-            <v-btn color="green darken-1" plain @click="showForm = true"
-              >Submit</v-btn
-            >
-          </v-card-actions>
+        <template v-slot:thumbnailUrl>
+          <v-text-field
+            v-model.trim="thumbnailUrl"
+            hint="Please enter a valid URL"
+            label="URL to book image"
+          />
         </template>
-      </v-card>
+        <template v-slot:authors>
+          <v-text-field
+            v-model="authors"
+            :rules="textRules"
+            counter="50"
+            label="Authors"
+          />
+        </template>
+        <template v-slot:price>
+          <v-text-field
+            v-model.number="price"
+            :rules="numRules"
+            counter="25"
+            hint="Please enter a valid integer"
+            label="Price (SGD)"
+          />
+        </template>
+        <template v-slot:shortDescription>
+          <v-text-field
+            v-model.trim="shortDescription"
+            :rules="descriptionRules"
+            counter="500"
+            label="Description"
+          />
+        </template>
+        <template v-slot:cancelBtn>
+          <v-btn text color="green darken-2" @click="closeDialog">
+            Close
+          </v-btn>
+        </template>
+        <template v-slot:submitBtn>
+          <v-btn color="green darken-2" text @click="prepDataAndSubmit()">
+            Update details
+          </v-btn>
+        </template>
+        <template v-slot:completionBtn>
+          <v-btn text color="green darken-2" @click="closeDialog">
+            Close
+          </v-btn>
+        </template>
+      </MultiStepForm>
     </v-dialog>
+    <v-snackbar v-model="snackbar" :timeout="timeout">
+      Book has been modified
+      <template v-slot:action="{ attrs }">
+        <!-- eslint-disable-next-line -->
+        <v-btn color="blue" text v-bind="attrs" @click="closeSnackBar">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
     <!-- </v-layout> -->
   </div>
   <!-- <v-overlay :value="overlay"></v-overlay> -->
 </template>
 
 <script>
-import PageHeader from "../HOCs/PageHeader.vue";
-import AllBooksTable from "./AllBooksTable.vue";
-import Card from "../HOCs/Card.vue";
+import PageHeader from '../HOCs/PageHeader.vue';
+import AllBooksTable from './AllBooksTable.vue';
+import MultiStepForm from '../HOCs/MultiStepForm.vue';
+import { mapActions } from 'vuex';
+
 // import { mapActions, mapGetters } from "vuex";
 // import Card from "../HOCs/Card.vue";
 export default {
   components: {
     PageHeader,
     AllBooksTable,
-    Card,
+    MultiStepForm,
   },
   data() {
     return {
+      // ====Snack bar=====
+      snackbar: false,
+      timeout: 5000,
+      // =====================
       valid: false,
       showOverlay: false,
       showDialog: false,
-      showForm: true,
-      showPreview: false,
-      authors: "",
+      // =====Form inputs=====
+      authors: '',
       price: 0,
-      shortDescription: "",
-      title: "",
-      thumbnailUrl: "",
-      rules: [
-        (v) => !!v || "This is a required field",
-        (v) => v.length <= 25 || "Max 25 characters",
+      shortDescription: '',
+      title: '',
+      thumbnailUrl: '',
+      isFormSubmitted: false,
+      id: '',
+      // =====================
+      // ==Validation rules===
+      textRules: [
+        (v) => !!v || 'This is a required field',
+        (v) => v.length <= 50 || 'Max 25 characters',
       ],
-      numRules: [(v) => !isNaN(v) || "Please enter a valid integer"],
-      // urlRules: [(v) => this.checkUrl(v) || "URL invalid"],
+      descriptionRules: [
+        (v) => !!v || 'This is a required field',
+        (v) => v.length <= 500 || 'Max 500 characters',
+      ],
+      numRules: [
+        (v) => !isNaN(v) || 'Please enter a valid integer',
+        (v) => v !== '' || 'Please enter a valid integer',
+      ],
     };
   },
   methods: {
@@ -169,23 +153,66 @@ export default {
     //   this.verifyUrl(v);
     //   return this.isUrlValid;
     // },
-
-    createPreview() {
-      this.showPreview = true;
-    },
-
+    ...mapActions('moduleUpdateBook', {
+      updateBook: 'updateBook',
+    }),
     openDialog() {
       this.showDialog = true;
     },
+    closeDialog() {
+      // Trigger the reset form method in the HOC
+      this.$refs.multiStepForm.resetForm();
+      // toggle isFormSubmitted so that when the card is opened next, it will display the cancel/submit instead of the post-submit btn
+      this.isFormSubmitted = false;
+      // Close the dialog
+      this.showDialog = false;
+    },
     validate() {
-      console.log("Validating the input....");
-      console.log("this.$ref.form.valdiate() is:");
       this.$refs.form.validate();
       this.createPreview();
     },
-    validateFormInput() {
-      console.log(this.$refs.form.valdiate());
-      this.createPreview();
+    // validateFormInput() {
+    //   console.log(this.$refs.form.valdiate());
+    //   this.createPreview();
+    // },
+    updateSelectedBook(selectedBook) {
+      this.title = selectedBook.title;
+      this.authors = selectedBook.authors;
+      this.shortDescription = selectedBook.shortDescription;
+      this.price = selectedBook.price;
+      this.thumbnailUrl = selectedBook.thumbnailUrl;
+      this.id = selectedBook.id;
+    },
+    prepDataAndSubmit() {
+      // Check if authors is an array, else convert from a string to an array of strings
+      let authorsArray = this.authors;
+      if (!Array.isArray(authorsArray)) {
+        authorsArray = authorsArray.split(',');
+      }
+
+      // Switch the state to show the form has been subitted
+      this.isFormSubmitted = true;
+
+      this.updateBook({
+        title: this.title,
+        thumbnailUrl: this.thumbnailUrl,
+        authors: authorsArray,
+        price: this.price,
+        shortDescription: this.shortDescription,
+        id: this.id,
+      });
+      this.snackbar = true;
+    },
+    resetFormFields() {
+      this.isFormSubmitted = false;
+      this.title = '';
+      this.thumbnailUrl = '';
+      this.authors = '';
+      this.price = null;
+      this.shortDescription = '';
+    },
+    closeSnackBar() {
+      this.snackbar = false;
     },
   },
   // computed: {
