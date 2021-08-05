3ad4958c695bc21d394b0c179b708d979f6ba187
Jeremy <jeremylim_91@live.com>
2021-08-05 19:21:28 +0800

    abstracted dialog into a hoc

---
+++ src/components/HOCs/Dialog.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-dialog v-model="openDialog" max-width="70%">
+    <slot name="dialogbody">
+      <v-card>
+        <v-card-title>
+          <slot name="dialogTitle"> This is the defaul card title</slot>
+        </v-card-title>
+        <v-card-text>
+          <slot name="dialogMessage"> This is the default card text</slot>
+        </v-card-text>
+      </v-card>
+    </slot>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: ["openDialog", "closeDialog"],
+};
+</script>
+
+<style></style>
